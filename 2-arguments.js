60fb2b053068a56a574a300dd1318cf7103546ee
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2025-09-09 07:33:39 +0200

    add 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+function argumentsCounter(...args){
+    if(args.length === 0) {
+        return 'No argument'
+    }else if(args.length === 1){
+        return 'Argument found'
+    }else{
+        return 'Arguments found'
+    }
+}
+console.log(argumentsCounter())
