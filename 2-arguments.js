564794eebf9ed1a7ef7d80c81b50bb7ac47ccc6d
Gedeon DUFITIMANA <gedeon.dufitimana2023@kepler.org>
2025-09-09 07:38:04 +0200

    corrected mistakes in file of 22arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,19 @@
-function argumentsCounter(...args){
-    if(args.length === 0) {
-        return 'No argument'
-    }else if(args.length === 1){
-        return 'Argument found'
-    }else{
-        return 'Arguments found'
-    }
+const args = process.argv.slice(2); 
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
-console.log(argumentsCounter())
+// function argumentsCounter(...args){
+//     if(args.length === 0) {
+//         return 'No argument'
+//     }else if(args.length === 1){
+//         return 'Argument found'
+//     }else{
+//         return 'Arguments found'
+//     }
+// }
+// console.log(argumentsCounter())
